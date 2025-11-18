64d4e7a99e71c621196be6bd320ff0403e1ff0d5
Giovanni Pereira <giovanni.pereira.15977@gmail.com>
2025-11-18 06:02:17 -0300

    path fix

--- js/backend-fake.js
+++ js/backend-fake.js
@@ -256,7 +256,7 @@ function atualizarHeader() {
         // Conteúdo do cabeçalho logado (Meus Datasets, Nome, Email e Sair)
         headerLogado.innerHTML = `
             <div style="display: flex; gap: 10px; align-items: center;">
-                <a href="../pages/user/dataset.html" class="btn-header" style="background-color: white; color: black;">Meus Datasets</a>
+                <a href="../../pages/user/dataset.html" class="btn-header" style="background-color: white; color: black;">Meus Datasets</a>
                 <div style="display: flex; flex-direction: column; align-items: flex-end;">
                     <h2 style="margin: 0; font-size: 18px;">Olá, ${user.nome}</h2>
                     <p style="margin: 0;"><strong>Email:</strong> <span id="perfilEmail">${user.email}</span></p>
